58722e2240e2165855e50c5c8b033ca9e1ae8bf0
Victor <mv.vitya@gmail.com>
2021-11-06 13:12:39 +0200

    final HW2

---
+++ HomeWork2/script.js
@@ -0,0 +1,31 @@
+let process = true;
+while (process) {
+  const n = prompt("Введіть порядковий номер числа послідовності Фібоначчі");
+  if (n === null) {
+    process = false;
+  } else if (!isNaN(+n) && n.length !== 0) {
+    fibonacciNumbers(0, 1, +n);
+    process = false;
+  }
+}
+function fibonacciNumbers(fFirst, fSecond, n) {
+  let fCurrentN = 0;
+  if (n === 1 || n === -1) fCurrentN += 1;
+  if (n >= 2) {
+    for (let i = 2; i <= n; i++) {
+      fCurrentN = fFirst + fSecond;
+      fFirst = fSecond;
+      fSecond = fCurrentN;
+    }
+  }
+  if (n <= -2) {
+    for (let i = -2; i >= n; i--) {
+      fCurrentN = fFirst - fSecond;
+      fFirst = fSecond;
+      fSecond = fCurrentN;
+    }
+  }
+  return alert(
+    `n-е число в узагальненій послідовності Фібоначчі = ${fCurrentN}`
+  );
+}
