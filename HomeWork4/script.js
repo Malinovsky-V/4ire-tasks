5d7a9b9e143d2bd86752b5694222cd643059b217
Malinovsky-V <86773044+Malinovsky-V@users.noreply.github.com>
2021-12-06 12:09:52 +0200

    Update script.js

--- HomeWork4/script.js
+++ HomeWork4/script.js
@@ -1,5 +1,9 @@
 const minNumberOfBuild = 1;
 
+function between (minNumber, maxNumber) {
+    return Math.floor(Math.random() * 
+    (maxNumber - minNumber) + minNumber)
+}
 // Power do Station 
 const powerStation = ()=>{
     const maxCountPowerStations = 6;
@@ -7,6 +11,7 @@ const powerStation = ()=>{
         const countStation = new Array(between(minNumberOfBuild, maxCountPowerStations)).fill(0)
             return countStation.reduce((power)=> power + between(1, maxPowerMakeStation), 0)
 }
+const countOfPowerInStationInAHalfDay = powerStation()/2
 
 // 1 Sun panels do 1 -5 only pm
 const sunPanelsInDay = (halfOfDay)=> {
@@ -16,14 +21,18 @@ const sunPanelsInDay = (halfOfDay)=> {
 }
 
 // numbrs of use appart
-const appartmentUseDay = (halfOfDay) => {
+const appartmentUseDay = () => {
     const maxNumberOfHouses = 1000;
     const maxNumbersOfAppartment = 400;
         const countOfHouses = between(minNumberOfBuild, maxNumberOfHouses);
             const countOfAppartment = countOfHouses * between(minNumberOfBuild, maxNumbersOfAppartment);
-                     return halfOfDay === 'day' ? countOfAppartment * 0.004 : countOfAppartment * 0.001
-
+                     return countOfAppartment
 }
+
+const countOfAppartments = appartmentUseDay()
+console.log(countOfAppartments)
+// halfOfDay === 'day' ? countOfAppartment * 0.004 : countOfAppartment * 0.001
+
 
 const linesPowerCounter = () => {
     const maxCountLines = 25;
@@ -31,52 +40,53 @@ const linesPowerCounter = () => {
     const minPowerLines = 1;
     const maxPrice = 46.3;
     const minPrice = 2.7;
-    const countLinesPower = new Array(between (minNumberOfBuild, maxCountLines)).fill(0)
-     countLinesPower.forEach((_,i) =>{ countLinesPower[i] = {
+        const countLinesPower = new Array(between (minNumberOfBuild, maxCountLines)).fill(0)
+            countLinesPower.forEach((_,i) =>{ countLinesPower[i] = {
             power: between (minPowerLines, maxPowerLines),
             priceForPower: (Math.random()* (maxPrice - minPrice) + minPrice).toFixed(2)
         }
     })
     return countLinesPower;
 }
+
 const linesPower = linesPowerCounter();
 
+// 
 const differencePowerInDay = () => {
-    return Math.floor((powerStation()/2 + sunPanelsInDay('day')) - appartmentUseDay('day'))
+    return (countOfPowerInStationInAHalfDay + sunPanelsInDay('day') - countOfAppartments * 0.004)
 }
+
 const differencePowerInNigth = () =>{
-    return Math.floor((powerStation()/2 + sunPanelsInDay('nigth')) - appartmentUseDay('nigth'))
+    return (countOfPowerInStationInAHalfDay + sunPanelsInDay('nigth') - countOfAppartments * 0.001)
 }
+
 const resultDayTime = {power:differencePowerInDay(), time:'day'}
 const resultNigthTime = {power: differencePowerInNigth(), time: 'nigth'}
 
-
-console.log(resultDayTime)
-// console.log(resultNigthTime)
-
-const analysisDifferent = (resultPositiveDayOrNigthTime) => {
-    if (resultPositiveDayOrNigthTime.power > 0) {
-        return sellingPower(resultPositiveDayOrNigthTime, linesPower)
+const analysisDifferent = (resultDayOrNigthTime) => {
+    if (resultDayOrNigthTime.power >= 0) {
+        const positiveInformation = sellingPower(resultDayOrNigthTime, linesPower)
+        return ` За ${positiveInformation.time}  місто отримало ${positiveInformation.power.toFixed(3)} мВт електроенергії
+        надлишкову потужність місто продало за  ${(positiveInformation.sumGet* 1000).toFixed(2)} ₴`
     }
-    if(resultPositiveDayOrNigthTime.power < 0){
-        return purchasePower(resultPositiveDayOrNigthTime, linesPower)
+    if(resultDayOrNigthTime.power < 0){
+        const negativeInformation = purchasePower(resultDayOrNigthTime, linesPower)
+        return ` За ${negativeInformation.time}  місто отримало ${negativeInformation.power.toFixed(3)} мВт електроенергії
+                 за недостачу напруги місто витратило на закупівлю  ${(negativeInformation.sumGet* 1000).toFixed(2)} ₴`
     }
-    }
+}
     
-    const purchasePower = (resultPositiveDayOrNigthTime, arrOflinesPower) => {
-        let numberGiveOut = resultPositiveDayOrNigthTime.power
-        console.log(numberGiveOut)
+const purchasePower = (resultNegativeDayOrNigthTime, arrOflinesPower) => {
+        let numberGiveOut = resultNegativeDayOrNigthTime.power
         let indexOfLines = 0;
         let sumGet = 0;
                 while ((-numberGiveOut) >= arrOflinesPower[indexOfLines].power ){
-                    console.log(numberGiveOut)
                      numberGiveOut += arrOflinesPower[indexOfLines].power
                      if(numberGiveOut > 0) numberGiveOut - arrOflinesPower[indexOfLines].power
-                     console.log(numberGiveOut)
                          sumGet += +arrOflinesPower[indexOfLines].priceForPower
                             indexOfLines !== arrOflinesPower.length-1 ?   indexOfLines++ : indexOfLines = 0
             }
-            return {power: resultPositiveDayOrNigthTime.power, time: resultPositiveDayOrNigthTime.time , sumGet, numberGiveOut}
+            return {power: resultNegativeDayOrNigthTime.power, time: resultNegativeDayOrNigthTime.time , sumGet, numberGiveOut}
         }
 
 const sellingPower = (resultPositiveDayOrNigthTime, arrOflinesPower) => {
@@ -90,34 +100,42 @@ const sellingPower = (resultPositiveDayOrNigthTime, arrOflinesPower) => {
     }
     return {power: resultPositiveDayOrNigthTime.power, time: resultPositiveDayOrNigthTime.time , sumGet, numberGiveOut}
 }
-const positiveResult = analysisDifferent(resultDayTime)
-console.log(analysisDifferent(resultDayTime))
-console.log(analysisDifferent(resultNigthTime))
-    console.log(positiveResult)
 
-// const analysisNightDifferent = () => {
 
-// }
-function between (minNumber, maxNumber) {
-    return Math.floor(Math.random() * 
-    (maxNumber - minNumber) + minNumber)
-}
 
-const outputInformationAboutPowerInCity = (positiveResult) =>{
-    return `Time: ${positiveResult.time} received: ${positiveResult.power} spend or earned: `
-}
-console.log(outputInformationAboutPowerInCity())
 
 // console.log(between(10, 50))
-
-console.log()
-// console.log(resultDayTime)
-// console.log(differencePowerInDay())
-// console.log(differencePowerInNigth())
-// console.log(linesPower)
 // console.log(powerStation())
+// console.log(countOfPowerInStationInAHalfDay)
+
 // console.log(sunPanelsInDay('day'))
+// console.log(sunPanelsInDay('nigth'))
 // console.log(appartmentUseDay('day'))
 // console.log(appartmentUseDay('nigth'))
+// console.log(linesPowerCounter())
+// console.log(linesPower)
+
+// console.log(differencePowerInDay())
+// console.log(differencePowerInNigth())
+// console.log(resultDayTime)
+// console.log(resultNigthTime)
+
+// 
+console.log(analysisDifferent(resultDayTime))
+console.log(analysisDifferent(resultNigthTime))
+// console.log(analysisDifferent(resultNigthTime))
+
+// console.log(purchasePower)
+// console.log(sellingPower)
+// 
+// console.log(positiveResult)
+
+
+// console.log(sunPanelsInDay('day'))
+
 // console.log(linesPower())
 // console.log(analysisDayDifferent())
+
+
+
+
